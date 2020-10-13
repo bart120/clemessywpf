85470eefca50bff176ac3865be6e7c9c429f9636
Vincent <vleclerc@inow.fr>
2020-10-13 17:05:36 +0200

    ObservableCollection

--- clemessywpf/ViewModels/PieceListViewModel.cs
+++ clemessywpf/ViewModels/PieceListViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using clemessywpf.Models;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace clemessywpf.ViewModels
 {
     public class PieceListViewModel : BaseViewModel
     {
-        public List<Piece> Pieces { get; set; } = new List<Piece>
+        public ObservableCollection<Piece> Pieces { get; set; } = new ObservableCollection<Piece>
         {
             new Piece{ Reference = "445GGT50", Maj = DateTime.Now, Statut = 1, Tarif = 1234},
             new Piece{ Reference = "445GGT51", Maj = DateTime.Now, Statut = 2, Tarif = 1546},
@@ -26,6 +30,26 @@ namespace clemessywpf.ViewModels
                 this.Changed();
             }
         }
+
+        public Piece SelectedPiece { get; set; }
+        public ICommand DeleteCommand { get; private set; }
+
+        public PieceListViewModel()
+        {
+            this.DeleteCommand = new RelayCommand(DeletePiece);
+        }
+        
+        
+
+        private void DeletePiece()
+        {
+            if(this.SelectedPiece != null)
+                this.Pieces.Remove(this.SelectedPiece);
+            //this.Pieces = new List<Piece>(this.Pieces);
+            //this.Changed("Pieces");
+
+        }
+
 
     }
 }
