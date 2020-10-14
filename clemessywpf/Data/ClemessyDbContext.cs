2977fee9182ec56890f28388d6a13eb214d87ac1
Vincent <vleclerc@inow.fr>
2020-10-14 13:54:32 +0200

    add connectionstrings

---
+++ clemessywpf/Data/ClemessyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clemessywpf.Data
+{
+    public class ClemessyDbContext : DbContext
+    {
+        public ClemessyDbContext()
+        {
+
+        }
+    }
+}
